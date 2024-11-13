0052b8fa2685d5790bcb06ee5cb45f041035f1c8
tony.to02 <nhattien11789@gmail.com>
2024-11-13 16:15:36 +0700

    commit

---
+++ config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                dark_cyan: 'hsl(158, 36%, 37%)',
+                cream: 'hsl(30, 38%, 92%)',
+                very_dark_blue: 'hsl(212, 21%, 14%)',
+                dark_grayish_blue: 'hsl(228, 12%, 48%)',
+                white: 'hsl(0, 0%, 100%)',
+            },
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif'],
+                fraunces: ['Fraunces', 'serif'],
+            },
+            minWidth: {
+                '256': '64rem'
+            }
+        }
+    }
+}
